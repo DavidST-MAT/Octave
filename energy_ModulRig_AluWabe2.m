1f0a2ce148e4d1d3559bcf99997227e358e3d044
David-ST <103434041+DavidST-MAT@users.noreply.github.com>
2023-09-15 10:44:52 +0200

    Update energy_ModulRig_AluWabe2.m

--- energy_ModulRig_AluWabe2.m
+++ energy_ModulRig_AluWabe2.m
@@ -1,93 +1,105 @@
-%file_path = 'miniTest.csv';
-file_path = 'ModulTest.csv'
-
-% Modul für Butterworth Filter
+% Modul for signal processing package
 pkg load signal
 
-% Daten ab Zeile 24 einlesen
-data = csvread(file_path, 23, 0);
+% Argumente abrufen
+args = argv();
+if length(args) < 1
+    error("CSV-Dateiname als Argument erforderlich.");
+end
 
-%Channel Description,Force-01,Force-02,Force-03,Angle-04,Acceleration-05,
+% CSV-Datei einlesen (erstes Argument)
+%csv_filename = args{1};
+csv_filename = 'ModulTest.csv';
+
+% Read data from csv-file
+try
+    data = csvread(csv_filename, 23, 0);
+catch
+    error('Error reading data from CSV file.');
+end
+
+
+% Extract time and angle data
 time = data(:, 1);
 angle_channel_4 = data(:, 5);
 
 
-% Berechnung der Winkelgeschwindigkeit (Δα/Δt)
-delta_t = diff(time);  % Zeitdifferenzen zwischen den Datenpunkten
-delta_alpha = diff(angle_channel_4);  % Differenzen zwischen den Winkelsignalen
-omega = delta_alpha ./ delta_t;  % Winkelgeschwindigkeit
+% Calculation of angular velocity (Δα/Δt)
+try
+    delta_t = diff(time);
+    delta_alpha = diff(angle_channel_4);
+    omega = delta_alpha ./ delta_t;
+catch
+    error('Error calculating angular velocity.');
+end
 
 clear kinetic_energy_values;
 
-% Beispielwert für m
-mass = 0.256;         % Mass in kilograms
-% Schleife über die Werte von omega und berechnen der kinetischen Energie
-for i = 1:length(omega)
-    disp(omega(i));
-    kinetic_energy_values(i) = (1/2) * mass * omega(i)^2;
 
+% Mass for AluWabe2 in kg
+mass = 0.256;
+
+% Calculate kinetic energy
+for i = 1:length(omega)
+    kinetic_energy_values(i) = (1/2) * mass * omega(i)^2;
 end
 
 
-
-
-% Generate Signal
-fs = 2000; %Sampling Frequency
-t = 0:1/fs:1; 
+% Define signal processing parameters (CFC60)
+num_samples = 10041; % Number of samples
+fs = 2000;
+duration = num_samples / fs; % Calculate the duration
+t = 0:(1/fs):(duration - 1/fs); % Create the time vector;
 order = 2; %Filter Order
-f_cutoff = 60; %Cutoff Frequency
+f_cutoff = 100; %Cutoff Frequency
 
 
 % Butterworth Filter
 [b, a] = butter(order, f_cutoff/(fs/2));
 
-% Zero-phase filtern mit filtfilt
-filtered_signal_zerophase = filtfilt(b, a, kinetic_energy_values);
-
+% Zero-phase filtering with filtfilt
+try
+    filtered_signal_zerophase = filtfilt(b, a, kinetic_energy_values);
+catch
+    error('Error applying Butterworth filter.');
+end
 
 
 % Signal plotten
 figure;
 
-%subplot(2, 1, 1);
-%plot(time(2:end), kinetic_energy_values);
-%hold on;
-%plot(time, data);
-%title('Energy');
-%xlabel('Time (s)');
-%ylabel('Amplitude');
-
-%legend('Enery', 'Angle');
-
-%subplot(2,1,2);
-%plot(time(2:end), filtered_signal_zerophase);
-%title('Zero-Phase Filtered Signal');
-%xlabel('Time (s)');
-%ylabel('Amplitude');
-
-
-% Linkes Achsenobjekt für die Energie
-ax1 = subplot(2, 1, 1);
+% Left-axis object for energy
+ax1 = gca; % Get the current axes
 plot(ax1, time(2:end), kinetic_energy_values, 'b');
 xlabel(ax1, 'Time(s)');
-ylabel(ax1, 'Energy', 'Color', 'b');
+ylabel(ax1, 'Energy(J)', 'Color', 'b');
 
-% Rechtes Achsenobjekt für den Winkel
+% Right-axis object for angle
 ax2 = axes('Position', get(ax1, 'Position'), 'XAxisLocation', 'top', 'YAxisLocation', 'right', 'Color', 'none');
 line(time, angle_channel_4, 'Parent', ax2, 'Color', 'r');
-ylabel(ax2, 'Angle', 'Color', 'r');
+ylabel(ax2, 'Angle(°)', 'Color', 'r');
 
 % Legende hinzufügen (optional)
-%legend(ax1, 'Energie', 'Location', 'northwest');
-%legend(ax2, 'Winkel', 'Location', 'northeast');
+%legend(ax1, 'Energy', 'Location', 'northwest');
+%legend(ax2, 'Angle', 'Location', 'northeast');
+
+% save diagram as png
+outputFolderPath = 'C:\Users\DavidStrucken\Desktop\SalontaFinal\ModulRig_AluWabe2\Output\';
+outputFileName = 'ModulRig_AluWabe2_Raw.png';
+outputFilePath = fullfile(outputFolderPath, outputFileName);
+saveas(gcf, outputFilePath, 'png');
 
 
-subplot(2,1,2);
+
+% Plot the filtered signal
+figure;
 plot(time(2:end), filtered_signal_zerophase, 'g--');
-title('Zero-Phase Filtered Signal');
+title('Zero-Phase Filtered Signal CFC60');
 xlabel('Time(s)');
-ylabel('Amplitude');
+ylabel('Energy(J)');
 
-
-% Diagramm als PNG speichern
-saveas(gcf, "Output/ModulRig_AluWabe2.png", 'png');
+% Save diagram as png
+outputFolderPath = 'C:\Users\DavidStrucken\Desktop\SalontaFinal\ModulRig_AluWabe2\Output\';
+outputFileName = 'ModulRig_AluWabe2_CFC60.png';
+outputFilePath = fullfile(outputFolderPath, outputFileName);
+saveas(gcf, outputFilePath, 'png');
