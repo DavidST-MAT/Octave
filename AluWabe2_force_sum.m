608938382c947bbd88dd3f544601ac00fe648865
David-ST <103434041+DavidST-MAT@users.noreply.github.com>
2023-09-22 10:25:06 +0200

    Update and rename force_ModulRig_AluWabe2.m to AluWabe2_force_sum.m

--- AluWabe2_force_sum.m
+++ AluWabe2_force_sum.m
@@ -26,40 +26,30 @@ force3_channel_3 = data(:, 4);
 % Force_res
 total_force = force1_channel_1 + force2_channel_2 + force3_channel_3;
 
-
-
 % Signal plotten
 figure;
 
-% Divide the graphic into 2x2 subgraphs, first subgraph
-subplot(2, 2, 1);
-plot(time, force1_channel_1);
-title('Force 1');
-xlabel('Time(s)');
-ylabel('Force(kN)');
-
-subplot(2, 2, 2);  % second  subgraph
-plot(time, force2_channel_2);
-title('Force 2');
-xlabel('Time(s)');
-ylabel('Force(kN)');
-
-subplot(2, 2, 3);  % third Untergrafik
-plot(time, force3_channel_3);
-title('Force 3');
-xlabel('Time(s)');
-ylabel('Force(kN)');
-
-subplot(2, 2, 4);  % fourth subgraph
 plot(time, total_force);
-title('Overall result');
-xlabel('Time(s)');
-ylabel('Force(kN)');
-
+grid on;
+title('Sum Force');
+xlabel('time[s]');
+ylabel('force[kN]');
 
 
 % save diagram as png
 outputFolderPath = 'C:\Users\DavidStrucken\Desktop\SalontaFinal\ModulRig_AluWabe2\Output\';
-outputFileName = 'AluWabe2_force.png';
+outputFileName = 'ModulRig_AluWabe2_SumForce_Raw.png';
 outputFilePath = fullfile(outputFolderPath, outputFileName);
 saveas(gcf, outputFilePath, 'png');
+
+
+% save diagram as csv
+outputFolderPath_csv = 'C:\Users\DavidStrucken\Desktop\SalontaFinal\ModulRig_AluWabe2\Output\';
+outputFileName_csv = 'ModulRig_AluWabe2_SumForce_Raw.csv';
+outputFilePath_csv = fullfile(outputFolderPath_csv, outputFileName_csv)
+
+sum_force_csv = [time, total_force];
+header = {'time[h]', 'force[kN]'};
+header = strjoin(header, ',');
+dlmwrite(outputFilePath_csv, header, 'delimiter', '');
+dlmwrite(outputFilePath_csv , sum_force_csv, '-append', 'delimiter', ',');
